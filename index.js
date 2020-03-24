18f3915d098d8856adece61566b784831a89798d
chris <christopherscaudill@gmail.com>
2020-03-24 07:48:43 -0400

    basic commands

--- index.js
+++ index.js
@@ -6,9 +6,43 @@ require('dotenv').config();
 const prefix = process.env.PREFIX;
 let ver = process.env.NODE_ENV;
 
-bot.on("Ready", () => {
+const prefix2 = "!";
+
+bot.on("ready", () => {
     console.log("Connection Acquired.");
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${bot.user.tag}!`);
+})
+
+bot.on("message", msg => {
+    let args = msg.content.substring(prefix2.length).split(" ");
+
+    switch (args[0]) {
+        case "help":
+            msg.reply("Command List: ");
+            if (args[1] === "info") {
+                msg.channel.send("Github repo - ghub\n ect. ");
+                break;
+            } else if (args[1] === "fun") {
+                msg.channel.send("Fun - friday\n ect. ");
+                break;
+            } else {
+                msg.channel.send("Add a category (!help + x)" + "\nList of categories: !cat");
+                break;
+            }
+            //-----------------------------------------------------------------------------------//
+            case "info":
+                msg.channel.send("Hello I'm Statbot here to help. Type !help for commands. :thumbsup:");
+                break;
+            case "cat":
+                msg.channel.send("Categories:\n All categories = all\n Fun commands = fun \n Information = info");
+                break;
+            case "friday":
+                msg.channel.send("https://youtu.be/4z95SAFud7w");
+                break;
+            case "ghub":
+                msg.reply("https://github.com/chrissc6/DiscordBot1/tree/master");
+                break;
+    }
 })
 
 
